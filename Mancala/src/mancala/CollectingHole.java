4c47505c84c7b11e8e110fed25006fe7ada03096
melissamclean <mcleanmm@beloit.edu>
2017-09-24 17:33:20 -0500

    pseudo code for CollectingHole class

--- Mancala/src/mancala/CollectingHole.java
+++ Mancala/src/mancala/CollectingHole.java
@@ -1,5 +1,16 @@
 package mancala;
 
 public class CollectingHole {
-
+	int numSeeds = 0;
+	
+	private int count(hole){
+		//use other method to get number of seeds and return it
+		//make in hole class
+		return numSeeds;
+	}
+	
+	public void displayCount(hole){
+		//print number of seeds in tally bin underneath it.
+		System.out.println(count(hole));
+	}
 }
