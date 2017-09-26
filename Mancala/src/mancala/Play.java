53c9f45703e18b961815791861e014655030a275
Ben Russell: <russellbj@beloit.edu>
2017-09-26 15:32:32 -0500

    Turn and Play class changed
    In Play class: added the Move() method. When the Move() method is
    called, the current player switches in the Turn class.
    
    In Turn class: Determines based on calls from Move() method whose turn
    it is, also determines the total number of turns taken. Use
    getCurrPlayer() to figure out whose turn it is, use getCurrTurn() to
    determine the total number of turns.

--- Mancala/src/mancala/Play.java
+++ Mancala/src/mancala/Play.java
@@ -1,5 +1,15 @@
 package mancala;
 
 public class Play {
-
+	
+	private Turn turn = new Turn();
+	private boolean turnTaken;
+	private int currPlayer = turn.getCurrPlayer();
+	
+	public void Move()
+	{
+		// Insert ability for a user to make a move here
+		turn.switchTurn();
+		currPlayer = turn.getCurrPlayer();
+	}
 }
--- Mancala/src/mancala/Turn.java
+++ Mancala/src/mancala/Turn.java
@@ -1,5 +1,25 @@
 package mancala;
 
 public class Turn {
-
+	private Play game = new Play();
+	private int currTurn = 1;
+	private int currPlayer = 1;
+	
+	public void switchTurn()
+	{
+			if(currPlayer == 1)
+			{
+				currPlayer = 2;
+			}
+			else if(currPlayer == 2)
+			{
+				currPlayer = 1;
+				currTurn++;
+			}
+	}
+	
+	public int getCurrPlayer()
+	{
+		return currPlayer;
+	}
 }
