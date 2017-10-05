e68d1ab569e6ed3b36d3df18f6661ac8d289ab55
Eric <32111009+salvie35@users.noreply.github.com>
2017-10-05 15:14:30 -0500

    Added comments to GameBoard

--- Mancala/src/mancala/GameBoard.java
+++ Mancala/src/mancala/GameBoard.java
@@ -1,26 +1,26 @@
 package mancala;
 
-public class GameBoard {
+public class GameBoard { // Basic template for gameboards
 	int holesPerRow;
 	Row rowOne, rowTwo;
 	CollectingHole tallyOne, tallyTwo;
 	public Board(int holes){
 		holesPerRow=holes;
-		rowOne=new Row(holesPerRow);
+		rowOne=new Row(holesPerRow); // May need to add more rows based on game, currently built for Wari
 		rowTwo=new Row(holesPerRow);
 		tallyOne=new CollectingHole();
 		tallyTwo=new CollectingHole();
 	}
-	public Row getRowOne(){
+	public Row getRowOne(){ // (Subject to change based on variations of game) returns the first row for moves
 		return rowOne;
 	}
-	public Row getRowTwo(){
+	public Row getRowTwo(){ // Returns second row (both to be called by other classes when moving or collecting
 		return rowTwo;
 	}
-	public CollectingHole getPlayerOneBin(){
+	public CollectingHole getPlayerOneBin(){ // return value in tally bin one
 		return tallyOne;
 	}
-	public CollectingHole getPlayerTwoBin(){
+	public CollectingHole getPlayerTwoBin(){ // return value in tally bin two (once again, subject to change per game)
 		return tallyTwo;
 	}
 }
