3bbed8b9b2e2c827d9228abaff449241222e233a
aaron <aaron@144.89.233.125>
2017-10-05 14:38:06 -0500

    some progress on seeds class

---
+++ Mancala/src/mancala/Seeds.java
@@ -0,0 +1,23 @@
+package mancala;
+
+public class Seeds {
+	
+	int holes = (getRowAmount() * getBinsPerRow()); //Taken from Rules class //Post-Wari: end-bins as holes
+	
+	public int holes[] = new int[holes];
+	
+	public void Populate() { //Populate initial board with starting seed values
+		int startingValue = getSeedsPerBin(); //Taken from Rules class
+		
+		for(int i=0; 0 < holes; i++) {
+			int seedsInHole = startingValue; 
+			
+		}
+	}
+	
+	
+	
+	
+	
+
+}
