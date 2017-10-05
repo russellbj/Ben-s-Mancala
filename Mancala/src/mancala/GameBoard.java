18362bd96ae9e3feb025ef89307bcd6072f568d9
feinermanjn <feinermanjn@beloit.edu>
2017-10-05 14:44:37 -0500

    Wrote initial Board class

--- Mancala/src/mancala/GameBoard.java
+++ Mancala/src/mancala/GameBoard.java
@@ -1,5 +1,26 @@
 package mancala;
 
 public class GameBoard {
-
+	int holesPerRow;
+	Row rowOne, rowTwo;
+	CollectingHole tallyOne, tallyTwo;
+	public Board(int holes){
+		holesPerRow=holes;
+		rowOne=new Row(holesPerRow);
+		rowTwo=new Row(holesPerRow);
+		tallyOne=new CollectingHole();
+		tallyTwo=new CollectingHole();
+	}
+	public Row getRowOne(){
+		return rowOne;
+	}
+	public Row getRowTwo(){
+		return rowTwo;
+	}
+	public CollectingHole getPlayerOneBin(){
+		return tallyOne;
+	}
+	public CollectingHole getPlayerTwoBin(){
+		return tallyTwo;
+	}
 }
