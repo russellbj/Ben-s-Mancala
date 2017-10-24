54bdf08ccab56ff9632ea10106124e154d53ab61
robachramowicz <achramowiczrm@beloit.edu>
2017-10-24 15:42:00 -0500

    added test class

---
+++ Mancala/src/mancala/CollectingHoleTest.java
@@ -0,0 +1,24 @@
+package mancala;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CollectingHoleTest {
+
+	@Test
+	public void testCollectingHole() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSeeds() {
+		fail("Not yet implemented");
+	}
+
+}
