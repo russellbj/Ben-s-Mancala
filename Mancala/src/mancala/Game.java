87f2341c8a6358d03a2084286665e065377a7aa4
Eric <32111009+salvie35@users.noreply.github.com>
2017-10-31 14:38:53 -0500

    Update Game.java

--- Mancala/src/mancala/Game.java
+++ Mancala/src/mancala/Game.java
@@ -2,34 +2,56 @@ package mancala;
 
 public class Game {
 
-	moveSeeds(){
-		Seeds.move(Row(Turn.getCurrPlayer)[clickedHole]);
-//		int numOfSeeds = Row(Turn.get)[clickedHole].getAmount;
-//		int nextHole = clickedHole + 1;
-//		Row(Turn.get)[clickedHole].removeAll;
-//		while(numOfSeeds > 0){
-//			RowX[nextHole].add;
-//			nextHole++;
-//		}
+	private static Seed seeds = new Seed();
+	public static Rules rules = new Rules();
+	public static Turn turn = new Turn();
+	private static GameBoard board = new GameBoard();
+	public static CollectingHole P1 = new CollectingHole();
+	public static CollectingHole P2 = new CollectingHole();
+	private static boolean pointsToWin;
+	private static boolean possibleMoves;
+	static int clickedHoleIndex;
+	
+	static boolean clickHole(){
+		
+		// Add clicking functionality that returns the index of the hole you have clicked on
+		// Pass that index into the parameter for MoveSeeds()
+		// the current parameter value of 5 is ARBITRARY AND SHOULD BE CHANGED ONCE WE CAN IDENTIFY THE CLICKED INDEX
+		
+		clickedHoleIndex = 5; // Set the value of this integer to the index that was clicked. 5 is placeholder
+		seeds.moveSeeds(clickedHoleIndex); // Pass the index of the clicked hole into MoveSeeds()
+		return true;
+	}
+	
+	public static string determineWinner(){
+		if (P1.displayCount > P2.displayCount){
+			return "Player 1 wins!"
+		}else if (P1.displayCount < P2.displayCount){
+			return "Player 2 wins!"
+		}else{
+			return "It's a tie!"
+		}
 	}
 
 	public static void setup(){
-  		rules();
-		board();
+  		rules.getGameRules("WARI");
 	}
 
 	public static void main(){
 		setup();
-		loop{
-			moveSeeds;
-			Turn.switchTurn();
-			if(possibleMoves = false || pointsToWin){
-				endGame();
+		while(true){
+			if(clickHole()){
+				seeds.moveSeeds(clickedHoleIndex);
+				turn.switchTurn();
+				if(possibleMoves == false || pointsToWin){
+					endGame();
+				}
 			}
 		}
 	}
 
-	public static void gameOver(){
-		shutdown;
+	public static void endGame(){
+		System.out.println("Game Over, " + determineWinner());
+		System.exit(1);
 	}
 }
