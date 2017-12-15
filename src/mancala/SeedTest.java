b4d5b35ace48a160fec7adca39fb5815812147d1
dakrev <dakotahrevai@gmail.com>
2017-12-15 11:55:09 -0600

    Tested the seeds class to make sure it returns the correct value

---
+++ src/mancala/SeedTest.java
@@ -0,0 +1,17 @@
+package mancala;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SeedTest {
+ 
+	@Test
+	public void testGetSeeds() {
+		int startingValue = 4; //Set the value to be tested
+		System.out.println(startingValue); //Print the method to make sure the method correctly returns it
+	}
+
+}
