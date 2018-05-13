e2b8d2531ef7d7beacedf76a160384f8c64845be
kingpabo <kingpabo121@gmail.com>
2018-05-13 18:39:59 -0500

    Added TieGame Test MAde changes to TieGame Class

--- src/mancala/TieGame.java
+++ src/mancala/TieGame.java
@@ -11,17 +11,14 @@ import javax.swing.JOptionPane;
 
 public class TieGame {
 
-	public TieGame() {
-		
-		// TODO Auto-generated constructor stub
-	}
+	ArrayList<int[]> prevStates = new ArrayList<int[]>();
+	
+
+	boolean tieConditionMet = false;
 
 	boolean tieGame(int [] gameBoard){
 		final int limit = 25;
 		int count = 0;
-
-		boolean tieConditionMet = false;
-		ArrayList<int[]> prevStates = new ArrayList<int[]>();
 
 		for(int i = 0; i < prevStates.size(); i++) {
 			if (prevStates.get(i).equals(gameBoard)){
@@ -54,5 +51,14 @@ public class TieGame {
 		}
 		return tieChoice;
 	}
+
+	public ArrayList<int[]> getPrevStates() {
+		return prevStates;
+	}
+
+	public void setPrevStates(ArrayList<int[]> prevStates) {
+		this.prevStates = prevStates;
+	}
+	
 }
 
---
+++ src/mancala/TieGameTest.java
@@ -0,0 +1,14 @@
+package mancala;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TieGameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
