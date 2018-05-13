7085e3c13a6b121e6a01b599b4e9d051522befd5
kingpabo <kingpabo121@gmail.com>
2018-05-13 18:43:27 -0500

    Added Tie Game test class

--- src/mancala/TieGameTest.java
+++ src/mancala/TieGameTest.java
@@ -1,14 +1,32 @@
 package mancala;
 
+/**
+ * @author Papa B., Nisha Bhatta, 
+ * 
+ */
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 
-public class TieGameTest {
+public class TieGameTest extends TieGame {
+	//	TieGame t = new TieGame();
+	int [] testBoard = {1,2,3,4,5};
 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented"); // TODO
-	}
+	public void originStatetest() {
+		boolean a = tieGame(testBoard);
+		if (a == true) {
+			System.out.println("true");
+		}else {
+			System.out.println("false");
+		}
 
+		fail("Not yet implemented"); 
+	}
+	
+	//TODO
+	
 }
